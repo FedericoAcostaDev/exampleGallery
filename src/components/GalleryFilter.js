d6769ef083c25ea42f986fc1011fcc4ad5ac2c81
Federico <fedeacos045@gmail.com>
2021-12-03 11:09:12 -0300

    implementing sass in navbar

--- src/components/GalleryFilter.js
+++ src/components/GalleryFilter.js
@@ -1,20 +1,21 @@
 import React from "react";
 
+import classes from "../css/navbar.module.scss";
 import "../css/filter.css";
 
-export const GalleryFilter = props => {
+export const GalleryFilter = (props) => {
   const { onFilterChange, userSelected, topSelected, filterOptions } = props;
   return (
-    <div className="filter__wrapper">
-      <div className="logo" />
-      <div className="row">
-        <div className="filter col-md-12">
-          <div className="filter__section">
+    <nav className={classes.nav}>
+      <div className={classes.nav__container}>
+        <a className={classes.nav__container__brand}>I-MAGE</a>
+        <div className={classes.nav__container_row}>
+          <div className={classes.nav__container_row_filter}>
             <label htmlFor="section">Section</label>
             <select
               id="section"
               name="section"
-              onChange={e => onFilterChange(e)}
+              onChange={(e) => onFilterChange(e)}
               defaultValue={filterOptions.section}
             >
               <option value="hot">Hot</option>
@@ -22,12 +23,12 @@ export const GalleryFilter = props => {
               <option value="user">User</option>
             </select>
           </div>
-          <div className="filter__section">
-            <label htmlFor="sort">Sort by</label>
+          <div className={classes.nav__container_filter}>
+            <label htmlFor="sort">Viral</label>
             <select
               id="sort"
               name="sort"
-              onChange={e => onFilterChange(e)}
+              onChange={(e) => onFilterChange(e)}
               defaultValue={filterOptions.sort}
             >
               <option value="viral">Viral</option>
@@ -36,12 +37,27 @@ export const GalleryFilter = props => {
               {userSelected && <option value="rising">Rising</option>}
             </select>
           </div>
-          <div className="filter__section">
-            <label htmlFor="section">Time range</label>
+          <div className={classes.nav__container_filter}>
+            <label htmlFor="section">Window</label>
             <select
               id="window"
               name="window"
-              onChange={e => onFilterChange(e)}
+              onChange={(e) => onFilterChange(e)}
+              defaultValue={filterOptions.window}
+            >
+              <option value="day">Day</option>
+              <option value="week">Week</option>
+              <option value="month">Month</option>
+              <option value="year">Year</option>
+              <option value="all">All</option>
+            </select>
+          </div>
+          <div className={classes.nav__container_filter}>
+            <label htmlFor="section">Sort by</label>
+            <select
+              id="window"
+              name="window"
+              onChange={(e) => onFilterChange(e)}
               defaultValue={filterOptions.window}
             >
               <option value="day">Day</option>
@@ -53,6 +69,6 @@ export const GalleryFilter = props => {
           </div>
         </div>
       </div>
-    </div>
+    </nav>
   );
 };
