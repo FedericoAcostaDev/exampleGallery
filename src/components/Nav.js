8e1e1b0cb0dda2c1fba9e9bf798a858e24d00b0f
Federico <fedeacos045@gmail.com>
2021-12-04 13:59:28 -0300

    added left sidebar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,31 +1,80 @@
 import React, { useState } from "react";
 
-import { FaBars } from "react-icons/fa";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
 
 import "../css/nav.css";
 
 export const Nav = (props) => {
   const { onFilterChange, userSelected, topSelected, filterOptions } = props;
-  const [sidebar, setSidebar] = useState(false);
+  const [state, setState] = React.useState({
+    right: false,
+  });
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
 
-  const showSidebar = () => setSidebar(!sidebar);
+    setState({ ...state, [anchor]: open });
+  };
+  const list = (anchor) => (
+    <Box
+      sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <ListItemIcon /> : <ListItemIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <ListItemIcon /> : <ListItemIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
   return (
     <div className="filter__wrapper">
       <div className="logo" />
       <div className="row">
         <div className="filter col-md-12">
-          <nav className="nav">
-            <navicon className="navIcon">
-              <FaBars />
-            </navicon>
-          </nav>
-          <sidebarnav className="sidebarNav">
-            <sidebarwrap className="sidebarWrap">
-              <navicon>
-                <FaBars />
-              </navicon>
-            </sidebarwrap>
-          </sidebarnav>
+          <div>
+            {["left"].map((anchor) => (
+              <React.Fragment key={anchor}>
+                <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+                <Drawer
+                  anchor={anchor}
+                  open={state[anchor]}
+                  onClose={toggleDrawer(anchor, false)}
+                >
+                  {list(anchor)}
+                </Drawer>
+              </React.Fragment>
+            ))}
+          </div>
           <div className="filter__section">
             <label htmlFor="section">Section</label>
             <select
