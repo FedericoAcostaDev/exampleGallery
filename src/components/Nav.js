2642b33f012c5e2d1430a78609683477fcdc4bda
Federico <fedeacos045@gmail.com>
2021-12-04 17:47:44 -0300

    added searchBar w/o functionality

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -13,7 +13,9 @@ import {
   withStyles,
   RadioGroup,
   Radio,
+  InputBase,
 } from "@material-ui/core";
+import { styled, alpha } from "@mui/material/styles";
 import ListItem from "@mui/material/ListItem";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
@@ -22,6 +24,7 @@ import "../css/nav.css";
 import { blue, green } from "@mui/material/colors";
 
 export const Nav = (props) => {
+  //Styles
   const CustomSwitch = withStyles({
     switchBase: {
       color: green[600],
@@ -35,8 +38,73 @@ export const Nav = (props) => {
     checked: {},
     track: {},
   })(Switch);
+  //Styles SearchBar
+  const Search = styled("div")(({ theme }) => ({
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(3),
+      width: "auto",
+    },
+  }));
+
+  const SearchIconWrapper = styled("div")(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  }));
+
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: "inherit",
+    "& .MuiInputBase-input": {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create("width"),
+      width: "100%",
+      [theme.breakpoints.up("md")]: {
+        width: "20ch",
+      },
+    },
+  }));
+
+  // Defining Funcions
   const { onFilterChange, userSelected, topSelected, filterOptions } = props;
   const [checked, setChecked] = React.useState(false);
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
 
   const [state, setState] = React.useState({
     right: false,
@@ -143,6 +211,13 @@ export const Nav = (props) => {
       <div className="logo" />
       <div className="row">
         <div className="filter col-md-12">
+          <Search>
+            <SearchIconWrapper></SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ "aria-label": "search" }}
+            />
+          </Search>
           <div>
             {["right"].map((anchor) => (
               <React.Fragment key={anchor}>
@@ -157,7 +232,14 @@ export const Nav = (props) => {
               </React.Fragment>
             ))}
           </div>
-          <div className="filter__section">
+        </div>
+      </div>
+    </div>
+  );
+};
+
+/*
+<div className="filter__section">
             <label htmlFor="section">Section</label>
             <select
               id="section"
@@ -199,8 +281,4 @@ export const Nav = (props) => {
               <option value="all">All</option>
             </select>
           </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+          */
